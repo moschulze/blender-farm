991daa9bcf8a939dbc41211744ad862ec01dfe4b
Moritz Schulze <mo.schulze@gmx.net>
2015-11-21 17:16:51 +0100

    implemented basic project

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,60 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('AppBundle::index.html.twig');
+    }
+
+    public function projectIndexAction()
+    {
+        $projects = $this->getDoctrine()->getRepository('AppBundle:Project')->findAll();
+        return $this->render('AppBundle::project_index.html.twig', array(
+            'projects' => $projects
+        ));
+    }
+
+    public function projectDetailAction($id)
+    {
+        $project = $this->getDoctrine()->getRepository('AppBundle:Project')->find($id);
+
+        if(is_null($project)) {
+            throw new NotFoundHttpException('Project with id ' . $id . ' not found');
+        }
+
+        return $this->render('AppBundle::project_detail.html.twig', array(
+            'project' => $project
+        ));
+    }
+
+    public function projectAddAction(Request $request)
+    {
+        $project = new Project();
+
+        if($request->getMethod() == Request::METHOD_POST) {
+            $project->setName($request->get('name'));
+            $project->setStatus(Project::STATUS_NEW);
+            $this->getDoctrine()->getManager()->persist($project);
+            $this->getDoctrine()->getManager()->flush();
+
+            /** @var UploadedFile $file */
+            $file = $request->files->get('file');
+            $filePath = __DIR__.'/../../../web/files/'.$project->getId().'/';
+            mkdir($filePath);
+            $file->move($filePath, 'project.blend');
+            return $this->redirectToRoute('project_index');
+        }
+
+        return $this->render('AppBundle::project_add_edit.html.twig', array(
+            'project' => $project
+        ));
     }
 }
---
+++ src/AppBundle/Entity/Project.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Project
+ */
+class Project
+{
+    const STATUS_NEW        = 'NEW';
+    const STATUS_WAITING    = 'WAITING';
+    const STATUS_RENDERING  = 'RENDERING';
+    const STATUS_PAUSED     = 'PAUSED';
+    const STATUS_STOPPED    = 'STOPPED';
+    const STATUS_FINISHED   = 'FINISHED';
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Project
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var string
+     */
+    private $status;
+
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Project
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
