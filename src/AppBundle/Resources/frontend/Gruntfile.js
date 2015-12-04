c779ef6e1f302dd3da81300cd4553d66db930cb6
Moritz Schulze <mo.schulze@gmx.net>
2015-12-04 20:43:56 +0100

    refactored js, added uglify task

--- src/AppBundle/Resources/frontend/Gruntfile.js
+++ src/AppBundle/Resources/frontend/Gruntfile.js
@@ -39,13 +39,34 @@ module.exports = function(grunt) {
                 src: ['bower_components/bootstrap-sass/assets/fonts/bootstrap/*'],
                 dest: '../public/fonts/bootstrap/'
             }
+        },
+        uglify: {
+            options: {
+                mangle: true
+            },
+            main: {
+                files: {
+                    '../public/script/main.js': [
+                        'bower_components/jquery/dist/jquery.js',
+                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+                    ]
+                }
+            },
+            detail: {
+                files: {
+                    '../public/script/project_detail.js': [
+                        'script/project_detail.js'
+                    ]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['sass:build', 'copy:fonts']);
+    grunt.registerTask('default', ['sass:build', 'copy:fonts', 'uglify:main', 'uglify:detail']);
     grunt.registerTask('dev', ['sass:dev']);
 };
