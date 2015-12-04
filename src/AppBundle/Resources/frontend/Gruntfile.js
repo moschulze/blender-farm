8ea7c59f0a0ec76592fc45a26196ab1735fcd2a9
Moritz Schulze <mo.schulze@gmx.net>
2015-12-04 08:05:48 +0100

    added frontend toolchain, refactored css to sass

---
+++ src/AppBundle/Resources/frontend/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            build: {
+                options: {
+                    sourceMap: false,
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    '../public/style/screen.css': 'style/screen.scss'
+                }
+            },
+            dev: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'expanded'
+                },
+                files: {
+                    '../public/style/screen.css': 'style/screen.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['style/*.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass:build']);
+    grunt.registerTask('dev', ['sass:dev']);
+};
