0d06b926d6bfee62ab3ef10592db75ab54c2d1de
Moritz Schulze <mo.schulze@gmx.net>
2015-12-04 08:07:47 +0100

    disabled source map for sass

--- src/AppBundle/Resources/frontend/Gruntfile.js
+++ src/AppBundle/Resources/frontend/Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
             },
             dev: {
                 options: {
-                    sourceMap: true,
+                    sourceMap: false,
                     outputStyle: 'expanded'
                 },
                 files: {
