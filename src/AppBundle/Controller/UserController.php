6f7f7d9fed8b6c1235246cc6163ebf7377a33252
Moritz Schulze <mo.schulze@gmx.net>
2015-12-06 12:44:36 +0100

    implemented adding and editing of users

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
@@ -37,6 +39,84 @@ class UserController extends Controller
             'users' => $users,
             'pages' => (ceil($userCount / $this->usersPerPage)),
             'page' => $page
+        ));
+    }
+
+    public function addAction(Request $request)
+    {
+        $user = new User();
+        $errors = array();
+
+        if($request->getMethod() == 'POST') {
+            $user->setUsername($request->get('username'));
+            $user->setEmail($request->get('email'));
+            $user->setPassword($request->get('password'));
+
+            if($request->get('admin') == 'true') {
+                $user->setRole('ROLE_ADMIN');
+            } else {
+                $user->setRole('ROLE_USER');
+            }
+
+            foreach($this->get('validator')->validate($user) as $error) {
+                $errors[$error->getPropertyPath()] = $error;
+            }
+
+            if(empty($errors)) {
+                $encoder = $this->get('security.password_encoder');
+                $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+
+                $doctrineManager = $this->getDoctrine()->getManager();
+                $doctrineManager->persist($user);
+                $doctrineManager->flush();
+
+                return $this->redirectToRoute('user_index');
+            }
+        }
+
+        return $this->render('AppBundle::user_add_edit.html.twig', array(
+            'user' => $user,
+            'errors' => $errors
+        ));
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        $doctrine = $this->getDoctrine();
+        $user = $doctrine->getRepository('AppBundle:User')->find($id);
+        $errors = array();
+
+        if($request->getMethod() == 'POST') {
+            $user->setUsername($request->get('username'));
+            $user->setEmail($request->get('email'));
+
+            if($request->get('admin') == 'true') {
+                $user->setRole('ROLE_ADMIN');
+            } else {
+                $user->setRole('ROLE_USER');
+            }
+
+            foreach($this->get('validator')->validate($user) as $error) {
+                $errors[$error->getPropertyPath()] = $error;
+            }
+
+            if(empty($errors)) {
+                $password = $request->get('password');
+                if(!empty($password)) {
+                    $encoder = $this->get('security.password_encoder');
+                    $user->setPassword($encoder->encodePassword($user, $request->get('password')));
+                }
+
+                $doctrineManager = $doctrine->getManager();
+                $doctrineManager->flush();
+
+                return $this->redirectToRoute('user_index');
+            }
+        }
+
+        return $this->render('AppBundle::user_add_edit.html.twig', array(
+            'user' => $user,
+            'errors' => $errors
         ));
     }
 }
